7d6d042ff2f66b2f4d423b051128b8db3a4766be
lyp1noff <lyp1noff@gmail.com>
2022-05-08 01:41:58 +0300

    added silver_ingot

--- src/main/java/com/lyp1noff/advancedsolarpanels/advancedSolarPanels.java
+++ src/main/java/com/lyp1noff/advancedsolarpanels/advancedSolarPanels.java
@@ -1,35 +1,30 @@
-package com.lyp1noff.advancedsolarpanel;
+package com.lyp1noff.advancedsolarpanels;
 
+import com.lyp1noff.advancedsolarpanels.item.ModItems;
 import com.mojang.logging.LogUtils;
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
-import java.util.stream.Collectors;
-
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(advancedSolarPanel.MOD_ID)
-public class advancedSolarPanel
+@Mod(advancedSolarPanels.MOD_ID)
+public class advancedSolarPanels
 {
     // Directly reference a slf4j logger
-    public static final String MOD_ID = "advancedsolarpanel";
+    public static final String MOD_ID = "advancedsolarpanels";
     private static final Logger LOGGER = LogUtils.getLogger();
 
-    public advancedSolarPanel()
+    public advancedSolarPanels()
     {
         // Register the setup method for modloading
         IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(eventBus);
+
         eventBus.addListener(this::setup);
 
         // Register ourselves for server and other game events we are interested in
